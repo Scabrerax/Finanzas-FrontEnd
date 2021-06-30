dd7d3279521b47bd47a47ceeb95bb360b52798ac
DanielaGuardia <dguardiacuervo@gmail.com>
2021-06-30 15:47:48 -0500

    footer

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './home.css'
 
 import {Navbar} from '../Navbar/Nabvar'
+import { Footer} from '../footer/Footer'
 
 export const Home = () => {
     // const tarjetas = [1,2,3,4,5]
     return (
         <div>
             <Navbar />
+            <h1>Hola putito</h1>
+            <Footer />
         </div>
     )
 }
