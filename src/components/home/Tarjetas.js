1e77dd0b46a4c4640b97d84d1e97281a954c1787
Sergio Cabrera <scabrera@unal.edu.co>
2021-07-03 13:28:41 -0500

    Cambios a los botones de ingreso y egreso

--- src/components/home/Tarjetas.js
+++ src/components/home/Tarjetas.js
@@ -7,19 +7,27 @@ export const Tarjetas = (home) => {
   const {mes,ingresos,egresos,balance} = tarjeta
 
   return (
-    <Link to='/tablas' className="mes">
+    <div className="mes">
       <div className="card dimensiones">
         <div className="card-body">
           <h4 className="card-title text-dark">{mes}</h4>
           <hr/>
-          <h5 className="card-title text-dark">Ingresos: </h5>
-          <h6 className="card-subtitle mb-2 text-muted">{ingresos}</h6>
-          <h5 className="card-title text-dark">Egresos:</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{egresos}</h6>
+          <Link to ='/Tablas' style={{ textDecoration: 'none'}}> 
+            <div className='diverso' >
+              <h5 className="card-title text-dark">Ingresos: </h5>
+              <h6 className="card-subtitle mb-2 text-muted">{ingresos}</h6>
+            </div>
+          </Link>
+          <Link to ='/Tablas' style={{ textDecoration: 'none'}}> 
+            <div className='diverso'>
+              <h5 className="card-title text-dark">Egresos:</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{egresos}</h6>
+            </div>
+          </Link>
           <h5 className="card-title text-dark">Balance</h5>
           <h6 className="card-subtitle mb-2 text-muted">{balance}</h6>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
